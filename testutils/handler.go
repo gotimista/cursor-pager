5b24bcc584ee4f4d8da1ddb2291d132030030a2a
blank <hayaken3007@i.softbank.jp>
2024-05-02 02:21:25 +0900

    remove assert response #1

--- testutils/handler.go
+++ testutils/handler.go
@@ -3,8 +3,6 @@ package testutils
 
 import (
 	"encoding/json"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -26,26 +24,6 @@ func AssertJSON(t *testing.T, want, got []byte) {
 	if diff := cmp.Diff(jg, jw); diff != "" {
 		t.Errorf("got differs: (-got +want)\n%s", diff)
 	}
-}
-
-// AssertResponse asserts that the response status code and body are equal to the expected values.
-func AssertResponse(t *testing.T, got *http.Response, status int, body []byte) {
-	t.Helper()
-	t.Cleanup(func() { _ = got.Body.Close() })
-	gb, err := io.ReadAll(got.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got.StatusCode != status {
-		t.Fatalf("want status %d, but got %d, body: %q", status, got.StatusCode, gb)
-	}
-
-	if len(gb) == 0 && len(body) == 0 {
-		// 期待としても実体としてもレスポンスボディがないので
-		// AssertJSONを呼ぶ必要はない。
-		return
-	}
-	AssertJSON(t, body, gb)
 }
 
 // LoadFile reads a file and returns its content.
